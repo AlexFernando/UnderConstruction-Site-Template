a370dda37fbfaf32ecabec5459ffd35ebcd98002
alexfernando <alex.fernando.801@gmail.com>
2020-09-01 17:55:57 +0000

    Under Construction Site - first commit

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    `gatsby-plugin-react-helmet`,
+    
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        // Directory with the strings JSON
+        path: `${__dirname}/src/intl`,
+        // Supported languages
+        languages: [`es`, `en`, `fr`],
+        // Default site language
+        defaultLanguage: `en`,
+        // Redirects to `/es` in the route `/`
+        redirect: false,
+      },
+    },
+    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from '../components/layout';
+import UnderConstruction from '../components/underConstruction';
+
+const IndexPage = () => (
+  <Layout>
+      <UnderConstruction />
+  </Layout>
+)
+
+export default IndexPage
